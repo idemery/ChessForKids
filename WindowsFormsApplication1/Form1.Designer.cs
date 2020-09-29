e804c3908ecc2227d45619ce1d5208b43fc9ef20
Islam Mostafa Eldemery <imeldemery@TCC-ICT.COM>
2020-09-29 13:23:02 +0300

    Removed unnecessary projects

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,18 +29,6 @@
         private void InitializeComponent()
         {
             this.panelBoard = new System.Windows.Forms.Panel();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-            this.buttonReset = new System.Windows.Forms.Button();
-            this.richTextBoxMoves = new System.Windows.Forms.RichTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
-            this.splitContainer2.Panel1.SuspendLayout();
-            this.splitContainer2.Panel2.SuspendLayout();
-            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelBoard
@@ -48,82 +36,20 @@
             this.panelBoard.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelBoard.Location = new System.Drawing.Point(0, 0);
             this.panelBoard.Name = "panelBoard";
-            this.panelBoard.Size = new System.Drawing.Size(726, 725);
+            this.panelBoard.Size = new System.Drawing.Size(731, 728);
             this.panelBoard.TabIndex = 2;
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.richTextBoxMoves);
-            this.splitContainer1.Size = new System.Drawing.Size(959, 794);
-            this.splitContainer1.SplitterDistance = 726;
-            this.splitContainer1.TabIndex = 3;
-            // 
-            // splitContainer2
-            // 
-            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Name = "splitContainer2";
-            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer2.Panel1
-            // 
-            this.splitContainer2.Panel1.Controls.Add(this.panelBoard);
-            // 
-            // splitContainer2.Panel2
-            // 
-            this.splitContainer2.Panel2.Controls.Add(this.buttonReset);
-            this.splitContainer2.Size = new System.Drawing.Size(726, 794);
-            this.splitContainer2.SplitterDistance = 725;
-            this.splitContainer2.TabIndex = 0;
-            // 
-            // buttonReset
-            // 
-            this.buttonReset.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonReset.Location = new System.Drawing.Point(0, 0);
-            this.buttonReset.Margin = new System.Windows.Forms.Padding(50);
-            this.buttonReset.Name = "buttonReset";
-            this.buttonReset.Size = new System.Drawing.Size(726, 65);
-            this.buttonReset.TabIndex = 0;
-            this.buttonReset.Text = "Reset";
-            this.buttonReset.UseVisualStyleBackColor = true;
-            // 
-            // richTextBoxMoves
-            // 
-            this.richTextBoxMoves.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBoxMoves.Location = new System.Drawing.Point(0, 0);
-            this.richTextBoxMoves.Name = "richTextBoxMoves";
-            this.richTextBoxMoves.ReadOnly = true;
-            this.richTextBoxMoves.Size = new System.Drawing.Size(229, 794);
-            this.richTextBoxMoves.TabIndex = 0;
-            this.richTextBoxMoves.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(959, 794);
-            this.Controls.Add(this.splitContainer1);
+            this.ClientSize = new System.Drawing.Size(731, 728);
+            this.Controls.Add(this.panelBoard);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Chess Teacher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chess for Kids!";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
-            this.splitContainer2.Panel1.ResumeLayout(false);
-            this.splitContainer2.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
-            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -131,10 +57,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panelBoard;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.SplitContainer splitContainer2;
-        private System.Windows.Forms.Button buttonReset;
-        private System.Windows.Forms.RichTextBox richTextBoxMoves;
     }
 }
 
